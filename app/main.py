ddc61b5197c090ca53a09edc2056378b26b0b43a
Lakshman Shanmugam <lakshman.s@sq1.security>
2026-01-07 19:07:09 +0530

    Inital Commit

--- app/main.py
+++ app/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from .database import engine, Base
 from .routers import users
 
-# create the sq lite tables automatically on startup
+
 app = FastAPI()
 
-Base.metadata.create_all(bind=engine) 
+Base.metadata.create_all(bind=engine) # create the sq lite tables automatically on startup
 
 app.include_router(users.router)
 
